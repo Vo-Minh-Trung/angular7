c08e9ab218fdaecb4e2ad565cbc5ad18e61ba4f8
Your Name <you@example.com>
2020-10-28 16:40:00 +0700

    Angular7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
+import { OrderService } from './shared/order.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
+  entryComponents:[OrderItemsComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/order.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Order } from './order.model';
+import { OrderItem } from './order-item.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  formData: Order;
+  orderItems: OrderItem[];
+
+  constructor(private http: HttpClient) { }
+
+  saveOrUpdateOrder() {
+    var body = {
+      ...this.formData,
+      OrderItems: this.orderItems
+    };
+    return this.http.post(environment.apiURL + '/Order/', body);
+  }
+
+  getOrderList() {
+    return this.http.get(environment.apiURL + '/Order/').toPromise();
+  }
+
+  getOrderByID(id:number):any {
+    return this.http.get(environment.apiURL + '/edit/'+id+'/').toPromise();
+  }
+
+  deleteOrder(id:number) {
+    return this.http.delete(environment.apiURL + '/edit/'+id+'/').toPromise();
+  }
+}
